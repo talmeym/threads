17f152b741dec5f110a4a4bd75a4b8cbd7ea31bb
talmeym <mtalmey@gmail.com>
2015-11-06 14:42:54 +0000

    lightened time colours

--- src/main/java/gui/ActionCellRenderer.java
+++ src/main/java/gui/ActionCellRenderer.java
@@ -43,44 +43,47 @@ public class ActionCellRenderer extends DefaultTableCellRenderer
 		x_component.setBackground(Color.WHITE);
 
 		List x_actionItems = ThreadHelper.getAllActiveActions(o_thread);
-        
         Item x_item = (Item) x_actionItems.get(row);
-        
-        Date x_dueDate = x_item.getDueDate();
-        Date x_now = new Date();
-        Date x_eightPmToday = getEightPmToday();
-        Date x_eightPmTomorrow = getEightPmTomorrow();        
-        
-        if(x_now.after(x_dueDate))
-        {
-            x_component.setBackground(Color.RED);
-        }
-        else if(x_dueDate.before(x_eightPmToday))
-        {
-            x_component.setBackground(Color.ORANGE);
-        }
-        else if(x_dueDate.before(x_eightPmTomorrow))
-        {
-            x_component.setBackground(Color.YELLOW);
-        }
-        else if((x_dueDate.getTime() - x_now.getTime()) < (1000 * 60 * 60 * 24 * 7))
-        {
-            x_component.setBackground(Color.GREEN);
-        }
-        else
-        {
-            x_component.setBackground(Color.WHITE);
-        }
+
+		setColourForTime(x_component, x_item);
 
 		if(isSelected) {
 			x_component.setForeground(Color.BLACK);
-			x_component.setBackground(Color.CYAN);
+			x_component.setBackground(new Color(155, 255, 255));
 		}
 
         return x_component;
     }
 
-    private Date getEightPmToday()
+	private void setColourForTime(Component x_component, Item x_item) {
+		Date x_dueDate = x_item.getDueDate();
+		Date x_now = new Date();
+		Date x_eightPmToday = getEightPmToday();
+		Date x_eightPmTomorrow = getEightPmTomorrow();
+
+		if(x_now.after(x_dueDate))
+		{
+			x_component.setBackground(new Color(255, 155, 155));
+		}
+		else if(x_dueDate.before(x_eightPmToday))
+		{
+			x_component.setBackground(new Color(255, 193, 90));
+		}
+		else if(x_dueDate.before(x_eightPmTomorrow))
+		{
+			x_component.setBackground(new Color(255, 255, 155));
+		}
+		else if((x_dueDate.getTime() - x_now.getTime()) < (1000 * 60 * 60 * 24 * 7))
+		{
+			x_component.setBackground(new Color(155,255,155));
+		}
+		else
+		{
+			x_component.setBackground(Color.WHITE);
+		}
+	}
+
+	private Date getEightPmToday()
     {
         Calendar x_calendar = Calendar.getInstance();
         x_calendar.set(Calendar.HOUR_OF_DAY, 20);
