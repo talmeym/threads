2db7cc259721b57391f32403ea30cc41f0c60529
talmeym <mtalmey@gmail.com>
2017-01-04 09:16:58 +0000

    tidied font adjustment

--- src/main/java/Threads.java
+++ src/main/java/Threads.java
@@ -1,19 +1,16 @@
 import data.*;
 import data.Thread;
-import gui.*;
+import gui.WindowManager;
 import util.*;
 
-import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
-import java.io.*;
+import java.io.File;
 
 public class Threads {
     public static void main(String[] args) {
-		doFontStuff();
+		FontUtil.standardiseFontSizes();
 
-		String x_dataFilePath = args.length > 0 ? args[0] : "threads.xml";
-		File x_dataFile = new File(x_dataFilePath);
+		File x_dataFile = new File(args.length > 0 ? args[0] : "threads.xml");
 		File x_settingsFile = new File(x_dataFile.getParentFile(), "threads.properties");
 		Thread x_topThread = x_dataFile.exists() ? Loader.loadDocumentThread(x_dataFile) : new Thread("Threads");
 
@@ -37,31 +34,4 @@ public class Threads {
 		});
 	}
 
-	public static void doFontStuff() {
-		String x_fontName = ((Font) UIManager.get("Button.font")).getFontName();
-		Font x_font = new Font(x_fontName, Font.PLAIN, 12);
-
-		UIManager.put("Button.font", x_font);
-		UIManager.put("CheckBox.font", x_font);
-		UIManager.put("RadioButton.font", x_font);
-		UIManager.put("ComboBox.font", x_font);
-		UIManager.put("Label.font", x_font);
-		UIManager.put("List.font", x_font);
-		UIManager.put("MenuBar.font", x_font);
-		UIManager.put("MenuItem.font", x_font);
-		UIManager.put("Menu.font", x_font);
-		UIManager.put("PopupMenu.font", x_font);
-		UIManager.put("OptionPane.font", x_font);
-		UIManager.put("Panel.font", x_font);
-		UIManager.put("ScrollPane.font", x_font);
-		UIManager.put("Viewport.font", x_font);
-		UIManager.put("TabbedPane.font", x_font);
-		UIManager.put("Table.font", x_font);
-		UIManager.put("TableHeader.font", x_font);
-		UIManager.put("TextField.font", x_font);
-		UIManager.put("TextPane.font", x_font);
-		UIManager.put("TitledBorder.font", x_font);
-		UIManager.put("ToolTip.font", x_font);
-		UIManager.put("Tree.font", x_font);
-	}
 }
---
+++ src/main/java/util/FontUtil.java
@@ -0,0 +1,35 @@
+package util;
+
+import java.awt.*;
+
+import static javax.swing.UIManager.*;
+
+public class FontUtil {
+	public static void standardiseFontSizes() {
+		String x_fontName = ((Font) get("Button.font")).getFontName();
+		Font x_font = new Font(x_fontName, Font.PLAIN, 12);
+
+		put("Button.font", x_font);
+		put("CheckBox.font", x_font);
+		put("RadioButton.font", x_font);
+		put("ComboBox.font", x_font);
+		put("Label.font", x_font);
+		put("List.font", x_font);
+		put("MenuBar.font", x_font);
+		put("MenuItem.font", x_font);
+		put("Menu.font", x_font);
+		put("PopupMenu.font", x_font);
+		put("OptionPane.font", x_font);
+		put("Panel.font", x_font);
+		put("ScrollPane.font", x_font);
+		put("Viewport.font", x_font);
+		put("TabbedPane.font", x_font);
+		put("Table.font", x_font);
+		put("TableHeader.font", x_font);
+		put("TextField.font", x_font);
+		put("TextPane.font", x_font);
+		put("TitledBorder.font", x_font);
+		put("ToolTip.font", x_font);
+		put("Tree.font", x_font);
+	}
+}
