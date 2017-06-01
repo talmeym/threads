9cbe44b366307177f167407df57bf714d6071a7f
talmeym <mtalmey@gmail.com>
2017-06-01 15:09:56 +0100

    fixed bug with action / reminder cell renderers

--- src/main/java/threads/gui/ThreadActionCellRenderer.java
+++ src/main/java/threads/gui/ThreadActionCellRenderer.java
@@ -5,12 +5,14 @@ import threads.data.Thread;
 
 import javax.swing.*;
 
+import static threads.gui.ColourConstants.s_selectedColour;
+
 class ThreadActionCellRenderer extends BaseCellRenderer<Thread, Item> {
 	@Override
 	public java.awt.Component getTableCellRendererComponent(JTable p_table, Object p_value, boolean p_isSelected, boolean p_hasFocus, int p_row, int p_col) {
 		java.awt.Component x_awtComponent = super.getTableCellRendererComponent(p_table, p_value, p_isSelected, p_hasFocus, p_row, p_col);
 		Item x_item = o_tableModel.getDataItem(p_row, p_col);
-		x_awtComponent.setBackground(getColourForTime(x_item.getDueDate()));
+		x_awtComponent.setBackground(p_isSelected ? s_selectedColour : getColourForTime(x_item.getDueDate()));
 		return x_awtComponent;
 	}
 }
--- src/main/java/threads/gui/ThreadReminderCellRenderer.java
+++ src/main/java/threads/gui/ThreadReminderCellRenderer.java
@@ -5,12 +5,14 @@ import threads.data.Thread;
 
 import javax.swing.*;
 
+import static threads.gui.ColourConstants.s_selectedColour;
+
 class ThreadReminderCellRenderer extends BaseCellRenderer<Thread, Reminder> {
 	@Override
 	public java.awt.Component getTableCellRendererComponent(JTable p_table, Object p_value, boolean p_isSelected, boolean p_hasFocus, int p_row, int p_col) {
 		java.awt.Component x_awtComponent = super.getTableCellRendererComponent(p_table, p_value, p_isSelected, p_hasFocus, p_row, p_col);
 		Reminder x_reminder = o_tableModel.getDataItem(p_row, p_col);
-		x_awtComponent.setBackground(getColourForTime(x_reminder.getDueDate()));
+		x_awtComponent.setBackground(p_isSelected ? s_selectedColour : getColourForTime(x_reminder.getDueDate()));
 		return x_awtComponent;
 	}
 }
