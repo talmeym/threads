4b5844bc49db1458e22dea1b4fa4ef54c7503e0d
talmeym <mtalmey@gmail.com>
2017-06-06 15:29:56 +0100

    typo

--- src/main/java/threads/data/Item.java
+++ src/main/java/threads/data/Item.java
@@ -18,8 +18,8 @@ public class Item extends ThreadItem<Reminder> implements HasDueDate {
         this(UUID.randomUUID(), new Date(), new Date(), true, text, p_dueDate, null, null, null);
     }
     
-    public Item(UUID id, Date p_creationDate, Date p_modifledDate, boolean p_active, String p_text, Date p_dueDate, String p_notes, List<Reminder> p_reminders, File p_docFolder) {
-        super(id, p_creationDate, p_modifledDate, p_active, p_text, p_reminders, (obj1, obj2) -> obj1.getDueDate().compareTo(obj2.getDueDate()), p_docFolder);
+    public Item(UUID id, Date p_creationDate, Date p_modifiedDate, boolean p_active, String p_text, Date p_dueDate, String p_notes, List<Reminder> p_reminders, File p_docFolder) {
+        super(id, p_creationDate, p_modifiedDate, p_active, p_text, p_reminders, (obj1, obj2) -> obj1.getDueDate().compareTo(obj2.getDueDate()), p_docFolder);
 		o_dueDate = p_dueDate;
 		o_notes = p_notes;
     }
